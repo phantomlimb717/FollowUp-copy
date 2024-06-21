8584db8a29473cdf1b387e2faf9f6e1247cc54ef
Aaron Baw <aaronbaw@gmail.com>
2024-06-21 14:25:36 +0100

    Initial Commit

---
+++ FollowUp/FollowUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollowUpApp.swift
+//  FollowUp
+//
+//  Created by Aaron Baw on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FollowUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
